07f00669e1faa58dcbf09a65ddfcd7598b59b4ce
gmarvis <sgmarvis@gmail.com>
2023-08-25 12:57:20 +0100

    first commit

---
+++ src/components/form.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+
+const UserForm = () => {
+  return (
+    <div className='container'>
+        
+      
+    </div>
+  )
+}
+
+export default UserForm
+
